a0dcac83196fdc40dcf7c41faf14ec7d5cd39322
KirbyStarKnight <Rachelsk8tes@gmail.com>
2025-01-26 15:21:16 -0700

    Add files via upload

---
+++ variables.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+<head>
+
+	<meta charset="utf-8">
+	<title>Variables</title>
+</head>
+<body>
+<?php // Script 2.3 - variables.php
+
+// My info:
+$firstname = "Rachel";
+$lastname = "Brandon";
+$address = "1242 Candy Cane Ln";
+$phone = "123-456-7899";
+
+// Print the address:
+print "<p>My info is: <br>$firstname
+<br>$lastname <br>$address <br>$phone</p>";
+
+?>
+</body>
+</html>
